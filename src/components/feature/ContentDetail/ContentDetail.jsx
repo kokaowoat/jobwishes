1d1b1351d22a0a528bc03f2fcc656264aafceeee
Eknarin Thirayothin <oat@cashnow.co.th>
2020-12-05 23:53:33 +0700

    [Update] ContentDetail component styling

--- src/components/feature/ContentDetail/ContentDetail.jsx
+++ src/components/feature/ContentDetail/ContentDetail.jsx
@@ -2,17 +2,53 @@ import './ContentDetail.scss';
 import React, { useState } from 'react';
 import PropsTypes from 'prop-types';
 import { Modal, Button } from 'antd';
+import {
+  BankFilled,
+  EnvironmentFilled,
+  SoundFilled,
+  ProfileFilled,
+} from '@ant-design/icons';
+import { JWButton } from '../../';
 
 const ContentDetail = props => {
-  const { data } = props;
+  const { data, isDisplay } = props;
+  // console.log('data.how_to_apply', data.how_to_apply);
   return (
     <Modal
-      title={data.title}
-      visible={props.isDisplay}
+      title={null}
+      visible={isDisplay}
       footer={null}
+      className="contentDetailContainer"
     >
-      <div dangerouslySetInnerHTML={{ __html: data.description }} />
-      <div dangerouslySetInnerHTML={{ __html: data.how_to_apply }} />
+      <div className="header">
+        <img src={data.company_logo ? data.company_logo : '/default_company.jpg'} />
+        <span className="title">{data.title}</span>
+      </div>
+      <hr />
+      <div className="detail">
+        <span><BankFilled /> </span>
+        <span className="description">{data.company}</span>
+      </div>
+      <div className="detail">
+        <span><EnvironmentFilled /> </span>
+        <span className="description">{data.location}</span>
+      </div>
+      <div className="detail">
+        <span><SoundFilled /> </span>
+        <span className="description">{data.type}</span>
+      </div>
+      <hr />
+      <div className="detail">
+        <span className="label">Description</span>
+        <div className="content" dangerouslySetInnerHTML={{ __html: data.description }} />
+      </div>
+      <hr />
+      <div className="footer">
+        <JWButton href={data.company_url} target="_blank">Visit Company</JWButton>
+        <JWButton>Apply</JWButton>
+      </div>
+      {/* TODO concat to link */}
+      {/* <div dangerouslySetInnerHTML={{ __html: data.how_to_apply }} />       */}
     </Modal>
   );
 };
