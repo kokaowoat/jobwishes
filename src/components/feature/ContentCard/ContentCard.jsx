421a64c14a941f69d2f6b963a97d34183d6994de
Eknarin Thirayothin <oat@cashnow.co.th>
2020-12-05 22:41:59 +0700

    [Update] - send company data as props to ContentCard component - display default_company image incase of error loaded image - fix styling

--- src/components/feature/ContentCard/ContentCard.jsx
+++ src/components/feature/ContentCard/ContentCard.jsx
@@ -1,53 +1,44 @@
 import './ContentCard.scss';
-import React from 'react';
+import React, { useState } from 'react';
 import PropsTypes from 'prop-types';
 import {
+  CalendarFilled,
   ShoppingFilled,
   EnvironmentFilled,
   SoundFilled
 } from '@ant-design/icons';
+import { formatDate } from '../../../helpers';
 
 const ContentCard = props => {
-  let temp = {
-    id: "286d8321-c5e5-4311-82b5-1756e5393199",
-    type: "Full Time",
-    url: "https://jobs.github.com/positions/286d8321-c5e5-4311-82b5-1756e5393199",
-    created_at: "Thu Dec 03 15:26:50 UTC 2020",
-    company: "Amenitiz",
-    company_url: "https://www.amenitiz.io/",
-    location: "Barcelona",
-    title: "Senior Backend Developer (RoR)",
-    description: "\u003cp\u003eAmenitiz is a fast-growing SaaS startup based in Barcelona and servicing over 1'500 customers in 24 months. Backed by top tier investors, Amenitiz is revolutionizing Hospitality software by centralizing all the hotelier needs in a single platform (website builder, PMS, channel manager, payment system, and much more).\u003c/p\u003e\n\u003cp\u003eWe’re passionate and ambitious about our mission and aim to promote a culture that cares deeply about its customers.\u003c/p\u003e\n\u003cp\u003eDespite the current global pandemic, we’re continuously growing and adapting to the situation, our team continues to rise stronger and that’s where you come in!\u003c/p\u003e\n\u003cp\u003eAre you a Software Engineer with extensive programming experience? Is your code fast, efficient, and reliable? Do you particularly enjoy working with amazing people and building something impacting? If so, let’s talk!\u003c/p\u003e\n\u003cp\u003eWe’re not looking for experts. We’re looking for smart people who can bring in the knowledge and the best processes to get things done. \u003c/p\u003e\n\u003cp\u003eAt Amenitiz, we are customer-centric, you will work on a day-to-day basis with our product team to design, architect, and implement our product. Our mission at Amenitiz is to support the digital transformation of Hospitality through a reliable SaaS solution offering a unique experience to its users.\u003c/p\u003e\n\u003cp\u003eThe whole Amenitiz team is based in Barcelona (our offices are in the heart of the city, near Plaza Cataluña); the Tech team counts 4 people, intending to grow to 12 people within the next 6 months.\u003c/p\u003e\n\u003cp\u003e\u003cstrong\u003eAs Senior Backend Developer, this position involves:\u003c/strong\u003e\u003c/p\u003e\n\u003cul\u003e\n\u003cli\u003eProduct development in Ruby/Rails of a hospitality-related web-based product.\u003c/li\u003e\n\u003cli\u003eIntegrations between our platform to 3rd party APIs.\u003c/li\u003e\n\u003cli\u003eWork in an Agile environment with strong attention to scalability (We are serving hundreds of thousands of unique visitors per month through our CMS).\u003c/li\u003e\n\u003cli\u003eDiscuss, question, challenge, and offer technical solutions to product needs alongside the Product and Design Teams.\u003c/li\u003e\n\u003cli\u003eBeing able to mentor, coach, and train other colleagues as a domain expert.\u003c/li\u003e\n\u003cli\u003eIdentify bottlenecks, bugs, and propose solutions.\u003c/li\u003e\n\u003c/ul\u003e\n\u003cp\u003e\u003cstrong\u003eRequired skills and experience:\u003c/strong\u003e\u003c/p\u003e\n\u003cul\u003e\n\u003cli\u003eBachelor's degree or equivalent practical experience.\u003c/li\u003e\n\u003cli\u003e6 years+ experience in web development.\u003c/li\u003e\n\u003cli\u003e3 years+ experience in Ruby.\u003c/li\u003e\n\u003cli\u003ePragmatism and obsession for simplicity, efficiency, and performance.\u003c/li\u003e\n\u003cli\u003eProven knowledge of OOP, databases, development, and test methodologies.\u003c/li\u003e\n\u003cli\u003eObsession for quality and testing.\u003c/li\u003e\n\u003cli\u003eEager to learn new technologies and frameworks.\u003c/li\u003e\n\u003cli\u003eStrong communication skills.\u003c/li\u003e\n\u003cli\u003eProactive, driven, entrepreneurial with a 'can do’ attitude.\u003c/li\u003e\n\u003c/ul\u003e\n\u003cp\u003e\u003cstrong\u003eBonus points for:\u003c/strong\u003e\u003c/p\u003e\n\u003cul\u003e\n\u003cli\u003eWorked in a fast-growing startup environment.\u003c/li\u003e\n\u003cli\u003eExperience in the hospitality industry.\u003c/li\u003e\n\u003c/ul\u003e\n\u003cp\u003e\u003cstrong\u003eOur stack:\u003c/strong\u003e\u003c/p\u003e\n\u003cul\u003e\n\u003cli\u003eRuby on Rails\u003c/li\u003e\n\u003cli\u003ePostgres and Redis\u003c/li\u003e\n\u003cli\u003eMemcached\u003c/li\u003e\n\u003cli\u003eSidekiq\u003c/li\u003e\n\u003cli\u003eHAproxy\u003c/li\u003e\n\u003cli\u003eHeroku\u003c/li\u003e\n\u003cli\u003eDigitalOcean\u003c/li\u003e\n\u003c/ul\u003e\n\u003cp\u003e\u003cstrong\u003eWhat do we offer?\u003c/strong\u003e\u003c/p\u003e\n\u003cul\u003e\n\u003cli\u003eA competitive salary.\u003c/li\u003e\n\u003cli\u003eAn amazing working environment, where you will be able to make a true contribution and get your voice heard.\u003c/li\u003e\n\u003cli\u003eThe opportunity to have a huge impact on developing a fast-growing business that is at a pivotal point in its growth to expand internationally.\u003c/li\u003e\n\u003cli\u003eThe team is growing fast and we offer plenty of career and development opportunities.\u003c/li\u003e\n\u003cli\u003eFlexibility to work from home part of the week.\u003c/li\u003e\n\u003c/ul\u003e\n",
-    how_to_apply: "\u003cp\u003eSend your application on \u003ca href=\"https://amenitiz.welcomekit.co\"\u003ehttps://amenitiz.welcomekit.co\u003c/a\u003e\u003c/p\u003e\n",
-    company_logo: "https://jobs.github.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBaEtTIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--1aba9150689b0bddbf9f38332bb396e13bea6c2c/0%20(1).png"
-  }
+  const { data } = props;
+
+
   return (
-    <div className="contentCard">
       {/* Header Section */}
       <div className="header">
-        <img src={temp.company_logo} />
+        <img src={data.company_logo ? data.company_logo : '/default_company.jpg'} />
         <div className="title">
-          <span className="name">{temp.company}</span>
-          <span className="createdAt">{temp.created_at}</span>
+          <span className="name">{data.company}</span>
+          <span className="createdAt"><CalendarFilled /> {formatDate(data.created_at)}</span>
         </div>
       </div>
       {/* Image Section */}
       <div className="imageSection">
-        <img src={temp.company_logo} />
+        <img src={data.company_logo ? data.company_logo : '/default_company.jpg'} />
       </div>
       {/* Short Detail ex. title, place, type*/}
       <div className="detailContainer">
         <div className="detail">
           <span><ShoppingFilled /> </span>
-          <span className="label">{temp.title}</span>
+          <span className="label">{data.title}</span>
         </div>
         <div className="detail">
           <span><EnvironmentFilled /> </span>
-          <span className="label">{temp.location}</span>
+          <span className="label">{data.location}</span>
         </div>
         <div className="detail">
           <span><SoundFilled /> </span>
-          <span className="label">{temp.type}</span>
+          <span className="label">{data.type}</span>
         </div>
       </div>
     </div>
