0c643241a4f6c8ffd9fe4d6ba54923ebd378a868
Eknarin Thirayothin <oat@cashnow.co.th>
2020-12-05 16:55:20 +0700

    [Update] setup app route to Home page as default

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import logo from './logo.svg';
 import './App.scss';
+import { Home } from './pages';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="temp">TEMPsssssss</p>
-        <p className="">TEMPsssssss</p>
-        <p className="temp3">TEMPsssssss</p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <p className="temp">TEMPsssssss</p>
-    </div>
+    <Home />
   );
 }
 
