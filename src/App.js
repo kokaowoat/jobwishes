c9171bea00ac47236c816cfa95532cddbc4a0473
Eknarin Thirayothin <oat@cashnow.co.th>
2020-12-05 14:49:33 +0700

    [Update] add package node-sass v. 4.14.1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p className="temp">TEMPsssssss</p>
+        <p className="">TEMPsssssss</p>
+        <p className="temp3">TEMPsssssss</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <p className="temp">TEMPsssssss</p>
     </div>
   );
 }
